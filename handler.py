e145e15b63c6695ceeecdf45ca286124bccf1fdf
Ian Luo <ian@ianluo.com>
2021-04-03 20:19:28 -0400

    Switch from env vars to event

--- handler.py
+++ handler.py
@@ -4,25 +4,22 @@ from venmo_api import Client
 from ynab_sdk import YNAB
 from transaction import Transaction
 
-# Venmo
-venmo_handle = os.environ['VENMO_HANDLE']
-venmo_access_token = os.environ['VENMO_TOKEN']
-venmo_client = Client(access_token=venmo_access_token)
 
-# YNAB
-ynab_key = os.environ['YNAB_KEY']
-budget_id = os.environ['BUDGET_ID']
-account_id = os.environ['YNAB_VENMO_ACCOUNT_ID']
-ynab_client = YNAB(ynab_key)
+def auth_venmo(event):
+    return Client(access_token=event['venmo_access_token'])
 
 
-def get_venmo_transactions():
+def auth_ynab(event):
+    return YNAB(event['ynab_key'])
+
+
+def get_venmo_transactions(venmo_client, venmo_handle, ynab_venmo_account_id):
     user_id = venmo_client.user.get_my_profile().id
     transactions = venmo_client.user.get_user_transactions(user_id=user_id)
-    return list(map(lambda x: Transaction(x, venmo_handle, account_id), transactions))
+    return list(map(lambda x: Transaction(x, venmo_handle, ynab_venmo_account_id), transactions))
 
 
-def record_new_transactions(venmo_transactions, existing_transactions):
+def record_new_transactions(venmo_transactions, existing_transactions, ynab_client, budget_id):
     imported_transaction = [transaction.serialize_ynab_transaction() for transaction in venmo_transactions]
 
     existing_transaction_as_set = {(transaction.date, transaction.amount, transaction.payee_name) for transaction in existing_transactions}
@@ -31,7 +28,7 @@ def record_new_transactions(venmo_transactions, existing_transactions):
     ynab_client.transactions.create_transactions(budget_id=budget_id, transactions=missing_transactions)
 
 
-def update_uncleared_transactions(venmo_transactions, existing_transactions):
+def update_uncleared_transactions(venmo_transactions, existing_transactions, ynab_client, budget_id):
     imported_transaction = [transaction.serialize_ynab_transaction() for transaction in venmo_transactions]
     imported_transaction_as_dict = {(transaction.date, transaction.amount, transaction.payee_name) for transaction in imported_transaction}
 
@@ -43,13 +40,25 @@ def update_uncleared_transactions(venmo_transactions, existing_transactions):
 
 
 def handler(event, context):
-    venmo_transactions = get_venmo_transactions()
-    existing_transactions = ynab_client.transactions.get_transactions(budget_id=budget_id).data.transactions
-    record_new_transactions(venmo_transactions, existing_transactions)
-    update_uncleared_transactions(venmo_transactions, existing_transactions)
+    venmo_client = auth_venmo(event)
+    ynab_client = auth_ynab(event)
+
+    venmo_transactions = get_venmo_transactions(venmo_client, event['venmo_handle'], event['account_id'])
+    existing_transactions = ynab_client.transactions.get_transactions(budget_id=event['budget_id']).data.transactions
+
+    record_new_transactions(venmo_transactions, existing_transactions, ynab_client, event['budget_id'])
+    update_uncleared_transactions(venmo_transactions, existing_transactions, ynab_client, event['budget_id'])
 
 
 if __name__ == "__main__":
-    handler(None, None)
+    payload = {
+        'venmo_handle': os.environ['VENMO_HANDLE'],
+        'venmo_access_token': os.environ['VENMO_TOKEN'],
+        'ynab_key': os.environ['YNAB_KEY'],
+        'budget_id': os.environ['BUDGET_ID'],
+        'account_id': os.environ['YNAB_VENMO_ACCOUNT_ID'],
+    }
+
+    handler(payload, None)
 
 
